26c559b4872d29d1475a705d22a0deb8f01404e3
sergioduquem15 <60938788+sergioduquem15@users.noreply.github.com>
2020-03-12 13:32:16 -0500

    Correccion de codigo c++ y datos para graficar.

--- Seguimientos/Seguimiento1/N1036654435/SegClasesCpp.cpp
+++ Seguimientos/Seguimiento1/N1036654435/SegClasesCpp.cpp
@@ -1,7 +1,8 @@
-//realizado por: Sergio Duque Mejía
+//realizado por: Sergio Duque Mejï¿½a
 
 #include <iostream>
 using namespace std;
+#include <cmath>
 
 class particula
 {
@@ -18,7 +19,7 @@ public:
 
 }P1,P2;
 
-void particula::SetValues(float x,float y,float z,float vx,float vy,float vz,float m,int c)
+void particula::SetValues(float x,float y,float Ax,float Ay,float Az,float z,float Vx,float Vy,float Vz,float m,int q)
 {
   X = x;
   Y = y;
@@ -35,9 +36,10 @@ void particula::SetValues(float x,float y,float z,float vx,float vy,float vz,flo
 
 float particula::evolA(int Bz,float d,float x1,float y1,float z1)
 {
-  AX=-(k*Carga**2*x1)/(M*d)+(Carga*VY*Bz)/M;
-  AY=-(k*Carga**2*y1)/(M*d)-(Carga*VX*Bz)/M;
-  AZ=-(k*Carga**2*z1)/(M*d);
+  int k=1;
+  AX=-(k*(Carga*Carga)*x1)/(M*d)+(Carga*VY*Bz)/M;
+  AY=-(k*(Carga*Carga)*y1)/(M*d)-(Carga*VX*Bz)/M;
+  AZ=-(k*(Carga*Carga)*z1)/(M*d);
 }
 
 float particula::evolV(float t)
@@ -54,6 +56,7 @@ float particula::evolP(float t)
   Z=Z+(VZ*t)+(0.5*AZ*(t*t));
 }
 
+
 int main()
 {
 
@@ -61,26 +64,42 @@ int main()
   int q=1;
   int Bz=10;
   float t=0.01;
-  int k=1;
+
   
-  p1.SetValues(0,0,0,0,0,0,0,0,0,m,q);
-  p2.SetValues(1,0,0,0,0,0,0,0,0,m,-q);
+  P1.SetValues(0,0,0,0,0,0,0,0,0,m,q);
+  P2.SetValues(1,0,0,0,0,0,0,0,0,m,-q);
+
+  //cout << "PARTICULA 1: " << " x=" << P1.X  <<  " y=" << P1.Y << " z=" << P1.Z << " vx=" << P1.VX << " vy=" << P1.VY << " vz=" << P1.VZ << " ax=" << P1.AX << " ay=" << P1.AY << " az=" << P1.AZ << " masa=" << P1.M << " carga=" << P1.Carga << endl;
+  
+  //cout << "PARTICULA 2: " << " x=" << P2.X  <<  " y=" << P2.Y << " z=" << P2.Z << " vx=" << P2.VX << " vy=" << P2.VY << " vz=" << P2.VZ << " ax=" << P2.AX << " ay=" << P2.AY << " az=" << P2.AZ << " masa=" << P2.M << " carga=" << P2.Carga << endl;
+
+  cout << "--------------------------------" << endl;
 
   for (int i=0; i<1000; i++)
     {
-      d=((p1.X-p2.X)**2+(p1.Y-p2.Y)**2+(p1.Z-p2.Z)**2)**(3/2);
-      x1=(p2.X-p1.X);
-      y1=(p2.Y-p1.Y);
-      z1=(p2.Z-p1.Z);
+      float d = pow(((P1.X-P2.X)*(P1.X-P2.X)+(P1.Y-P2.Y)*(P1.Y-P2.Y)+(P1.Z-P2.Z)*(P1.Z-P2.Z)),3/2);
+      float x1=(P2.X-P1.X);
+      float y1=(P2.Y-P1.Y);
+      float z1=(P2.Z-P1.Z);
 
-      p1.evoA(Bz,d,x1,y1,z1);
-      p2.evoA(Bz,d,x1,y1,z1);
+      P1.evolA(Bz,d,x1,y1,z1);
+      P2.evolA(Bz,d,x1,y1,z1);
 
-      p1.evoV(t);
-      p2.evoV(t);
+      P1.evolV(t);
+      P2.evolV(t);
 
-      p1.evoP(t);
-      p2.evoP(t);
+      P1.evolP(t);
+      P2.evolP(t);
+
+      cout << P2.Y << endl;
+
+      
+
+     
+      //cout << "PARTICULA 1: " << " x=" << P1.X  <<  " y=" << P1.Y << " z=" << P1.Z << " vx=" << P1.VX << " vy=" << P1.VY << " vz=" << P1.VZ << " ax=" << P1.AX << " ay=" << P1.AY << " az=" << P1.AZ << " masa=" << P1.M << " carga=" << P1.Carga << endl;
+
+      //cout << "PARTICULA 2: " << " x=" << P2.X  <<  " y=" << P2.Y << " z=" << P2.Z << " vx=" << P2.VX << " vy=" << P2.VY << " vz=" << P2.VZ << " ax=" << P2.AX << " ay=" << P2.AY << " az=" << P2.AZ << " masa=" << P2.M << " carga=" << P2.Carga << endl;
 
     }
+  return 0;
 }
