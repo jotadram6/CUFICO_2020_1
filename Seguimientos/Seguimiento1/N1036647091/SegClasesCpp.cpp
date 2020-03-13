0d8261f45a8369c0ba3b988c14ade44813c4c8d8
lquiceno <lucas.quiceno@udea.edu.co>
2020-03-13 00:22:43 -0500

    Correcion_tarea_clases

--- Seguimientos/Seguimiento1/N1036647091/SegClasesCpp.cpp
+++ Seguimientos/Seguimiento1/N1036647091/SegClasesCpp.cpp
@@ -1,52 +1,69 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
-
-
-
-
-
+//Como B=10 y M=10 y  W=Bq/M entonces W=Q=1 
 class Particula{
 public:
   int Carga;
   float X,Y,Z;
-  float VX,VY,VZ;
+  float Vx,Vy,Vz;
   float M;
-  void SetValues(float,float,float,float,float,float,float,int);
-  float Pos_evol(float);
+  float Bz;
+  void SetValues(float,float,float,float,float,float,float,float,int);
+  float Pos_evol(float,float,float,float);
   
   
 }Par1,Par2;
 
-void Particula::SetValues(float x,float y,float z,float vx,float vy,float vz,float m, int C)
+void Particula::SetValues(float x,float y,float z,float vx,float vy,float vz,float m, float bz, int C)
 {
   X=x;
   Y=y;
   Z=z;
-  VX=vx;
-  VY=vy;
-  VZ=vz;
+  Vx=vx;
+  Vy=vy;
+  Vz=vz;
   M=m;
+  Bz=bz;
   Carga=C;
- }
-
-float Particula::Pos_evol(float t){
-  X=sin(t);
-  Y=cos(t)-1;
-  Z=t;
 }
-int main(){ 
-    
-    Par1.SetValues(1.0,0.0,0.0,1.0,0.0,0.0,1.0,1);
 
-   
-  
-    
-    for (int i=0; i<10000; i++){
-      Par1.Pos_evol(i/0.01);
-
-    
-      cout <<  Par1.X <<","<< Par1.Y  <<","<< Par1.Z  << endl;}
-  return 0;
+float Particula::Pos_evol(float Fx,float Fy, float Fz,float t){
+  float w=Bz*Carga/M;
+ 
+  X=X+(Vx*t)+(0.5*Fx*(t*t));
+  Y=Y+(Vy*t)+(0.5*Fx*(t*t));
+  Z=Z+(Vz*t)+(0.5*Fx*(t*t));
+  Vx=Vx+(Fx*t/M);
+  Vy=Vy+(Fy*t/M);
+  Vz=Vz+(Fz*t/M);
 }
+
+int main(){
+
+  //Evolucion de la particula 1 y 2
+  Par1.SetValues(0.0,0.0,0.0,0.0,0.0,0.0,10.0,10.0,1);
+  Par2.SetValues(1.0,0.0,0.0,0.0,0.0,1.0,10.0,10.0,1);
+  for (int i=0; i<10000; i++){
+
+    if ((pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2))==0) break;
+      
+    float Fx1=Par1.Vy-1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    float Fy1=-Par1.Vx-1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    float Fz1=1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+
+    float Fx2=Par1.Vy-1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    float Fy2=-Par1.Vx-1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    float Fz2=1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    
+    Par1.Pos_evol(Fx1,Fy1,Fz1,i*0.01);
+    Par2.Pos_evol(Fx2,Fy2,Fz2,i*0.01);
+
+    
+    std::cout <<    Par1.X << "," << Par1.Y  << "," << Par1.Z  << "," <<    Par2.X << "," << Par2.Y  << "," << Par2.Z  << std::endl;
+  }
+  return 0; }	
+
+
+
+
--- Seguimientos/Seguimiento1/N1036647091/SegClasesPython.py
+++ Seguimientos/Seguimiento1/N1036647091/SegClasesPython.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
-from mpl_toolkits import mplot3d
-get_ipython().run_line_magic('matplotlib', 'inline')
+#!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-# In[23]:
+class Particle:
 
-
-#!/usr/bin/python
-class Particula:
     
-    cargada= True
+    cargada = True
+
     
-    def __init__(self,x,y,z,vx,vy,vz,m,carga):
+
+    def __init__(self, x, y, z, vx, vy, vz, m, carga):
+
         self.X=x
         self.Y=y
         self.Z=z
@@ -27,27 +21,87 @@ class Particula:
         self.VZ=vz
         self.M=m
         self.Carga=carga
-    def Pos_evol(self,t):
-        self.X=np.sin(t)
-        self.Y=(np.cos(t)-1)
-        self.Z=t
-Par1=Particula(1.0,0.0,0.0,1.0,1.0,1.0,1.0,1)
-
-for i in range(1,10000,1):
-
-    Par1.Pos_evol(0.1)
-    print(Par1.X,Par1.Y,Par1.Z)
-    
     
 
+    def Fuerza(self, Ex, Ey, Ez):
 
-# In[ ]:
+        BZ = 10.0
+        Fx = self.Carga*(Ex + self.VY*BZ)
+        Fy = self.Carga*(Ey - self.VX*BZ)
+        Fz = self.Carga*Ez
+
+        return Fx,Fy,Fz
+
+        
+    def Vel_evol(self, ax, ay, az, t):
+
+        self.VX = self.VX + ax*t
+        self.VY = self.VY + ay*t
+        self.VZ = self.VZ + az*t
+
+
+    def Pos_evol(self, ax, ay, az, t):
+
+       self.X = self.X + (self.VX * t) + (0.5*ax*t*t)
+       self.Y = self.Y + (self.VY * t) + (0.5*ay*t*t)
+       self.Z = self.Z + (self.VZ * t) + (0.5*az*t*t)
+       
+
+P1 = Particle(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0, 1)
+P2 = Particle(1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0, -1)
 
 
 
+x1 = []
+y1 = []
+z1 = []
+
+x2 = []
+y2 = []
+z2 = []
+
+for i in range(0, 10000):
+    if ((P2.X-P1.X)**(2) + (P2.Y-P1.Y)**(2) + (P2.Z-P1.Z)**(2)) == 0: break
+
+    Ex2 = (P2.Carga*(P1.X-P2.X))/(4*np.pi*(8.8541e-12)*((P1.X-P2.X)**(2) + (P1.Y-P2.Y)**(2) + (P1.Z-P2.Z)**(2))**(3/2))
+    Ey2 = (P2.Carga*(P1.Y-P2.Y))/(4*np.pi*8.8541e-12*((P1.X-P2.X)**(2) + (P1.Y-P2.Y)**(2) + (P1.Z-P2.Z)**(2))**(3/2))
+    Ez2 = (P2.Carga*(P1.Z-P2.Z))/(4*np.pi*8.8541e-12*((P1.X-P2.X)**(2) + (P1.Y-P2.Y)**(2) + (P1.Z-P2.Z)**(2))**(3/2))
+    
+    Ex1 = (P1.Carga*(-P1.X+P2.X))/(4*np.pi*8.8541e-12*((P2.X-P1.X)**(2) + (P2.Y-P1.Y)**(2) + (P2.Z-P1.Z)**(2))**(3/2))
+    Ey1 = (P1.Carga*(-P1.Y+P2.Y))/(4*np.pi*8.8541e-12*((P2.X-P1.X)**(2) + (P2.Y-P1.Y)**(2) + (P2.Z-P1.Z)**(2))**(3/2))
+    Ez1 = (P1.Carga*(-P1.Z+P2.Z))/(4*np.pi*8.8541e-12*((P2.X-P1.X)**(2) + (P2.Y-P1.Y)**(2) + (P2.Z-P1.Z)**(2))**(3/2))
+
+    Fx, Fy, Fz =P1.Fuerza(Ex2, Ey2, Ez2)
+    Fx2, Fy2, Fz2 =P2.Fuerza(Ex1, Ey1, Ez1)
+    
+    P1.Vel_evol(Fx/P1.M, Fy/P1.M, Fz/P1.M, 0.1)
+    P1.Pos_evol(Fx/P1.M, Fy/P1.M, Fz/P1.M, 0.1)
+    P2.Vel_evol(Fx2/P2.M, Fy2/P2.M, Fz2/P2.M, 0.1)
+    P2.Pos_evol(Fx2/P2.M, Fy2/P2.M, Fz2/P2.M, 0.1)
+ 
+
+    x1.append(P1.X)
+    y1.append(P1.Y)
+    z1.append(P1.Z)
+
+    x2.append(P2.X)
+    y2.append(P2.Y)
+    z2.append(P2.Z)
+    
 
 
-# In[ ]:
+fig = plt.figure(figsize = (10,10))
+ax = Axes3D(fig)
+ax.plot(x1,y1,z1)
+ax.plot(x2,y2,z2)
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+
+plt.legend()
+plt.show()
 
 
 
